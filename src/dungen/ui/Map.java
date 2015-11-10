211a65ff2e8ab816a29f0e3a469eca6c7da20ae1
Justin Risch <JRisch@ipponusa.com>
2015-11-10 11:15:33 -0500

    Halls and star fixed

--- src/dungen/ui/Map.java
+++ src/dungen/ui/Map.java
@@ -1,14 +1,14 @@
 package dungen.ui;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.Point;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
 
 public class Map extends JFrame {
 
@@ -16,11 +16,17 @@ public class Map extends JFrame {
 	public ArrayList<Point> roomsLocations = new ArrayList<>();
 	public ArrayList<JLabel> rooms = new ArrayList<>();
 	public ArrayList<JLabel> halls = new ArrayList<>();
-	public Component star = new JLabel("*");
-	public transient JPanel contentPane = new JPanel();
+	public JLabel star = new JLabel("*");
+	private transient final static ImageIcon roomImage = new ImageIcon(
+			"Resources/Room.png");
+	private transient final static ImageIcon hallImage = new ImageIcon(
+			"Resources/Hall.png");
+	private transient final static ImageIcon hallImage2 = new ImageIcon(
+			"Resources/Hall2.png");
+	public transient JLayeredPane contentPane = new JLayeredPane();
 	private transient static final Font font = new Font(Font.MONOSPACED, 0, 9);
 	{
-		contentPane.setBackground(Color.WHITE);
+		this.setBackground(Color.BLACK);
 		star.setSize(5, 5);
 		star.setFont(font);
 	}
@@ -35,11 +41,12 @@ public class Map extends JFrame {
 	}
 
 	public void addRoom(Integer x, Integer y) {
-		Point p = new Point((contentPane.getWidth() / 2) + x * 20,
-				this.getHeight() / 2 - y * 20);
+		Point p = new Point((contentPane.getWidth() / 2) + x * 30,
+				this.getHeight() / 2 - y * 30);
 		if (roomsLocations != null && !roomsLocations.contains(p)) {
-			JLabel room = new JLabel("[]");
-			room.setSize(10, 8);
+			JLabel room = new JLabel();
+			room.setIcon(roomImage);
+			room.setSize(15, 15);
 			room.setLocation((int) p.getX(), (int) p.getY());
 			room.setFont(font);
 			room.setVisible(true);
@@ -52,11 +59,11 @@ public class Map extends JFrame {
 	}
 
 	public void moveStar(int x, int y) {
-		Point p = new Point((this.getWidth() / 2) + (x * 20) + 3,
-				(this.getHeight() / 2) - (y * 20) + 2);
+		Point p = new Point((this.getWidth() / 2) + (x * 30) + 5,
+				(this.getHeight() / 2) - (y * 30) + 5);
 		star.setLocation(p);
 		star.setVisible(true);
-		contentPane.add(star);
+		contentPane.add(star, contentPane.highestLayer());
 		star.repaint();
 	}
 
@@ -65,27 +72,33 @@ public class Map extends JFrame {
 	}
 
 	public void addHall(Integer x, Integer y, String direction, boolean hidden) {
-		Point p = new Point((this.getWidth() / 2) + x * 20,
-				(this.getHeight() / 2) - y * 20);
+		Point p = new Point((this.getWidth() / 2) + x * 30,
+				(this.getHeight() / 2) - y * 30);
 		int modX = 0, modY = 0;
 		JLabel room = rooms.get(roomsLocations.indexOf(p));
-		JLabel hall = new JLabel("+");
-		if (hidden)
+		JLabel hall = new JLabel();
+		if (hidden) {
 			hall.setText("S");
+		} else if (direction.equalsIgnoreCase("north")
+				|| direction.equalsIgnoreCase("south")) {
+			hall.setIcon(hallImage);
+		} else {
+			hall.setIcon(hallImage2);
+		}
 		hall.setFont(font);
-		hall.setSize(10, 10);
+		hall.setSize(15, 15);
 		if (direction.equalsIgnoreCase("north")) {
-			modX = room.getX() + 3;
+			modX = room.getX();
 			modY = room.getY() - room.getHeight();
 		} else if (direction.equalsIgnoreCase("south")) {
-			modX = room.getX() + 3;
-			modY = room.getY() + room.getHeight() - 3;
+			modX = room.getX();
+			modY = room.getY() + room.getHeight();
 		} else if (direction.equalsIgnoreCase("east")) {
-			modX = room.getX() + room.getWidth() - 2;
-			modY = room.getY() - 1;
+			modX = room.getX() + room.getWidth();
+			modY = room.getY();
 		} else if (direction.equalsIgnoreCase("west")) {
-			modX = room.getX() - 3;
-			modY = room.getY() - 1;
+			modX = room.getX() - hall.getWidth();
+			modY = room.getY();
 		}
 		hall.setLocation(modX, modY);
 

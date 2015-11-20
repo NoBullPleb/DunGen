4c07b04ffb1e0d0f5180d649adf986b545b8c1dc
Justin Risch <JRisch@ipponusa.com>
2015-11-20 16:38:32 -0500

    new Custom(String) roller to make hoard tables clearer

--- src/dungen/generators/Dice.java
+++ src/dungen/generators/Dice.java
@@ -8,6 +8,18 @@ public class Dice {
 		return result;
 	}
 
+	public static String custom(String s) {
+		int result = 0;
+		String[] params = s.split("d");
+		int dice = Integer.parseInt(params[0]);
+		int sides = Integer.parseInt(params[1].split(" ")[0]);
+		String items = s.substring(params.length + 1
+				+ params[1].split(" ").length);
+		for (int i = 0; i < dice; i++)
+			result += (int) Math.floor(Math.random() * sides) + 1;
+		return result + " " + items;
+	}
+
 	public static int custom(int sides) {
 		return custom(1, sides);
 	}

de5853007f366500a006c39f123257b8e1189c91
Justin Risch <JRisch@ipponusa.com>
2015-11-26 17:00:58 -0500

    loader improvements

--- src/dungen/resourceLoader/ResourceLoader.java
+++ src/dungen/resourceLoader/ResourceLoader.java
@@ -24,24 +24,20 @@ public class ResourceLoader {
 	}
 
 	private static Path getResourcePath(String path) throws Exception {
-		try {
-			Optional<Path> paths = Files
-					.find(table, 2, (a, b) -> a.toString().contains(path))
-					.parallel().findFirst();
-			if (!paths.isPresent()) {
-				System.out.println("No matches found for " + path);
-				return null;
-			}
-			return paths.get();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
+		Optional<Path> paths = Files
+				.find(table, 2, (a, b) -> a.toString().contains(path))
+				.parallel().findFirst();
+		if (!paths.isPresent()) {
+			System.out.println("No matches found for " + path);
+			throw new Exception("No matches found for resource: " + path);
 		}
+		return paths.get();
 	}
 
 	public static List<String> getTable(String path) {
 		try {
 			return Files.readAllLines(getResourcePath(path));
+
 		} catch (Exception e) {
 			System.out.println("FAILED- " + path + " : " + e.toString() + ", "
 					+ e.getStackTrace()[0].getLineNumber());

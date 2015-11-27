e4e7304aa642dca60fbd011f91d78b7154448e3e
Justin Risch <JRisch@ipponusa.com>
2015-11-27 04:27:48 -0500

    removed unnecessary bits

--- src/dungen/resourceLoader/ResourceLoader.java
+++ src/dungen/resourceLoader/ResourceLoader.java
@@ -1,10 +1,8 @@
 package dungen.resourceLoader;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,19 +11,9 @@ import javax.swing.ImageIcon;
 
 public class ResourceLoader {
 	static final ClassLoader cl = ResourceLoader.class.getClassLoader();
-	static String tableString = cl.getResource("dungen").getPath();
-	static final Path table;
-
-	static {
-		tableString = tableString.substring(0, tableString.lastIndexOf("/"))
-				.replaceAll("%20", " ").replaceAll("file:", "");
-		table = new File(tableString).toPath();
-		System.out.println("table string: " + tableString);
-	}
 
 	public static ImageIcon getImage(String path) {
 		try {
-
 			ImageIcon i = new ImageIcon(cl.getResource("images/" + path));
 			System.out.println("Found image: "
 					+ cl.getResource("images/" + path).getPath());
